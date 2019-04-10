2b504fa30d63bad1d7ed0fd9ad394474f9f8688c
Chris Ringrose <C.Ringrose2@newcastle.ac.uk>
2019-04-10 16:02:18 +0100

    Unittest tweak

--- QUBEKit/tests/ligand_tests.py
+++ QUBEKit/tests/ligand_tests.py
@@ -7,15 +7,15 @@ class TestLigands(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.molecule = Ligand('tests/test_files/acetone.pdb')
+        cls.molecule = Ligand('test_files/acetone.pdb')
 
     def test_pdb_reader(self):
 
         # Check all atoms are found
-        self.assertEqual(10, len(self.molecule.molecule))
+        self.assertEqual(10, len(self.molecule.molecule['input']))
 
         # Check atom names and coords are extracted for each atom in the molecule
-        for atom in self.molecule.molecule:
+        for atom in self.molecule.molecule['input']:
             self.assertEqual(4, len(atom))
 
 
