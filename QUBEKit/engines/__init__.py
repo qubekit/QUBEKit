8739d4b6e6dd34c8a0902f13ac2465ef679911cc
Chris Ringrose <C.Ringrose2@newcastle.ac.uk>
2019-08-16 14:35:35 +0100

    Added environment yml explanation; clear comments for decorator; removed guff from environment yml

--- QUBEKit/engines/__init__.py
+++ QUBEKit/engines/__init__.py
@@ -5,6 +5,8 @@ from .chargemol import Chargemol
 from QUBEKit.utils.exceptions import try_load
 
 
+# Ensures that errors with particular engines are only raised if said engine is being used.
+# No point raising "cannot execute psi4 command" if user is using g09. See try_load docstring for info.
 Gaussian = try_load('Gaussian', 'QUBEKit.engines.gaussian')
 ONETEP = try_load('ONETEP', 'QUBEKit.engines.onetep')
 OpenMM = try_load('OpenMM', 'QUBEKit.engines.openmm')
