34513373cc87e7a7760211fc685d26adbdd5a404
bieniekmat <bieniekmat@gmail.com>
2022-07-07 13:16:22 +0100

    Basic integration of the first fragmenter openff-fragmenter.

---
+++ qubekit/fragmentation/WBOFragmenter.py
@@ -0,0 +1,73 @@
+from typing_extensions import Literal
+from typing import List, Optional, Union
+
+from pydantic import Field
+from qcelemental.util import which_import
+
+from qubekit.molecules import Ligand
+from qubekit.utils.datastructures import StageBase
+
+
+class WBOFragmenter(StageBase):
+
+    type: Literal["WBOFragmenter"] = "WBOFragmenter"
+
+    rotatable_smirks: List[str] = Field(
+        ["[!#1]~[!$(*#*)&!D1:1]-,=;!@[!$(*#*)&!D1:2]~[!#1]"],
+        description="The rotatable smirks to use for fragmentation",
+    )
+
+    @classmethod
+    def is_available(cls) -> bool:
+        fragmenter = which_import(
+            "openff.fragmenter",
+            return_bool=True,
+            raise_error=True,
+            raise_msg="Please install via `conda install -c conda-forge openff-fragmenter`.",
+        )
+
+        bespokefit = which_import(
+            "openff.bespokefit",
+            return_bool=True,
+            raise_error=True,
+            raise_msg="Please install via `conda install -c conda-forge openff-bespokefit`.",
+        )
+
+        return fragmenter and bespokefit
+
+    def run(self, molecule: Ligand, **kwargs) -> Ligand:
+        from openff.toolkit.topology import Molecule
+        # from openff.bespokefit.executor.services.fragmenter.worker import _deduplicate_fragments
+        from openff.fragmenter.depiction import depict_fragmentation_result
+        from openff.fragmenter.fragment import WBOFragmenter
+
+        fragmenter = WBOFragmenter()
+
+        mapped_smiles = molecule.to_smiles(mapped=True)
+        off_molecule: Molecule = Molecule.from_mapped_smiles(mapped_smiles)
+
+        result = fragmenter.fragment(off_molecule, target_bond_smarts=self.rotatable_smirks)
+
+        # deduplicated_result = _deduplicate_fragments(result).json()
+
+        molecule.fragments = {Ligand.from_smiles(fragment.smiles, 'fragment'): fragment.bond_indices
+                              for fragment
+                              in result.fragments}
+
+        # visualise results
+        depict_fragmentation_result(result=result, output_file="fragments.html")
+
+        return molecule
+
+    def start_message(self, **kwargs) -> str:
+        """
+        A friendly message to let users know that stage is starting with any important options.
+        """
+        # fixme - check if the fragmenter uses the same default smirks?
+        return "WBOFragmenter is starting the job"
+
+    def finish_message(self, **kwargs) -> str:
+        """
+        A friendly message to let users know that the stage is complete and any checks that have been performed.
+        """
+        return f"WBOFragmenter has ended"
---
+++ qubekit/tests/fragmentation/wbofragmenter_test.py
@@ -0,0 +1,18 @@
+
+from qubekit.molecules import Ligand
+from qubekit.fragmentation import WBOFragmenter
+
+
+def test_generate_fragments():
+    """
+    Generate fragments for the example case Cobimetinib
+    """
+    Cobimetinib = "OC1(CN(C1)C(=O)C1=C(NC2=C(F)C=C(I)C=C2)C(F)=C(F)C=C1)[C@@H]1CCCCN1"
+    molecule = Ligand.from_smiles(Cobimetinib, 'Cobimetinib')
+
+    fragmenter = WBOFragmenter()
+    result = fragmenter.run(molecule)
+
+    # Cobimetinib has 5 fragments according to the example
+    # https://docs.openforcefield.org/projects/fragmenter/en/latest/fragment-molecules.html
+    assert len(result.fragments) == 5
