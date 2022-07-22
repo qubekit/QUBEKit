dfe406c304bbbe4dbf3a7fb342dad81ca54817cc
Josh Horton <Josh.Horton@newcastle.ac.uk>
2022-07-22 17:11:49 +0100

    remove logging

--- qubekit/torsions/fitting/forcebalance_wrapper.py
+++ qubekit/torsions/fitting/forcebalance_wrapper.py
@@ -437,7 +437,6 @@ class ForceBalanceFitting(StageBase):
         converged = False
         with open("optimize.out") as log:
             for line in log.readlines():
-                print(line)
                 if "optimization converged" in line.lower():
                     converged = True
                     break
