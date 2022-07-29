d23296c4356758da7a88c8f2bf760dcbd04e6187
bieniekmat <bieniekmat@gmail.com>
2022-07-29 11:04:51 +0100

    Saving fragments as ligands in the molecule. Deduplication is absorbed from bespoke-fit.

--- qubekit/fragmentation/WBOFragmenter.py
+++ qubekit/fragmentation/WBOFragmenter.py
@@ -1,11 +1,18 @@
+import itertools
+from typing import List, TYPE_CHECKING
 from typing_extensions import Literal
-from typing import List, Optional, Union
 
 from pydantic import Field
 from qcelemental.util import which_import
 
 from qubekit.molecules import Ligand
 from qubekit.utils.datastructures import StageBase
+
+if TYPE_CHECKING:
+    from openff.toolkit.topology import Molecule
+    from openff.fragmenter.fragment import (
+        FragmentationResult,
+    )
 
 
 class WBOFragmenter(StageBase):
@@ -26,12 +33,18 @@ class WBOFragmenter(StageBase):
             raise_msg="Please install via `conda install -c conda-forge openff-fragmenter`.",
         )
 
-        return fragmenter
+        rdkit = which_import(
+            "rdkit",
+            return_bool=True,
+            raise_error=True,
+            raise_msg="Please install via `conda install -c conda-forge rdkit`.",
+        )
+
+        return fragmenter and rdkit
 
     def run(self, molecule: Ligand, **kwargs) -> Ligand:
         from openff.toolkit.topology import Molecule as OFFMolecule
 
-        # from openff.bespokefit.executor.services.fragmenter.worker import _deduplicate_fragments
         from openff.fragmenter.depiction import depict_fragmentation_result
         from openff.fragmenter.fragment import WBOFragmenter
 
@@ -42,21 +55,96 @@ class WBOFragmenter(StageBase):
         off_molecule: OFFMolecule = OFFMolecule.from_mapped_smiles(mapped_smiles)
 
         # fragment
-        result = fragmenter.fragment(
+        fragmentation = fragmenter.fragment(
             off_molecule, target_bond_smarts=self.rotatable_smirks
         )
 
-        # deduplicated_result = _deduplicate_fragments(result).json()
+        # remove duplicates
+        fragmentation = self._deduplicate_fragments(fragmentation)
 
-        # simpler representation of the fragments
-        molecule.fragments = {
-            fragment.smiles: fragment.bond_indices for fragment in result.fragments
-        }
+        # save the fragments in the molecule as ligand objects
+        fragments = []
+        for fragment in fragmentation.fragments:
+            ligand = Ligand.from_smiles(fragment.smiles, "fragment")
+            ligand.bond_indices = fragment.bond_indices
+            fragments.append(ligand)
+        molecule.fragments = fragments
 
         # visualise results in a file
-        depict_fragmentation_result(result=result, output_file="fragments.html")
+        depict_fragmentation_result(result=fragmentation, output_file="fragments.html")
 
         return molecule
+
+    def _deduplicate_fragments(
+        self, fragmentation_result: "FragmentationResult"
+    ) -> "FragmentationResult":
+        """Remove symmetry equivalent fragments from the results."""
+        unique_fragments = []
+
+        # group by the same smiles
+        for smile, fragments in itertools.groupby(
+            fragmentation_result.fragments,
+            lambda frag: frag.molecule.to_smiles(explicit_hydrogens=False),
+        ):
+            fragments = list(fragments)
+
+            if len(fragments) == 1:
+                unique_fragments.extend(fragments)
+                continue
+
+            symmetry_groups = set()
+            for fragment in fragments:
+                bond_map = fragment.bond_indices
+                fragment_mol = fragment.molecule
+                # get the index of the atoms in the fragment
+                atom1, atom2 = self._get_atom_index(
+                    fragment_mol, bond_map[0]
+                ), self._get_atom_index(fragment_mol, bond_map[1])
+                symmetry_classes = self._rd_get_atom_symmetries(fragment_mol)
+                symmetry_group = tuple(
+                    sorted([symmetry_classes[atom1], symmetry_classes[atom2]])
+                )
+                if symmetry_group not in symmetry_groups:
+                    symmetry_groups.add(symmetry_group)
+                    unique_fragments.append(fragment)
+
+        fragmentation_result.fragments = unique_fragments
+
+        return fragmentation_result
+
+    def _rd_get_atom_symmetries(self, molecule: "Molecule") -> List[int]:
+
+        from rdkit import Chem
+
+        rd_mol = molecule.to_rdkit()
+        return list(Chem.CanonicalRankAtoms(rd_mol, breakTies=False))
+
+    def _get_atom_index(self, molecule: "Molecule", map_index: int) -> int:
+        """Returns the atom index of the atom in a molecule which has the specified map
+        index.
+
+        Parameters
+        ----------
+        molecule
+            The molecule containing the atom.
+        map_index
+            The map index of the atom in the molecule.
+
+        Returns
+        -------
+            The corresponding atom index
+        """
+        inverse_atom_map = {
+            j: i for i, j in molecule.properties.get("atom_map", {}).items()
+        }
+
+        atom_index = inverse_atom_map.get(map_index, None)
+
+        assert (
+            atom_index is not None
+        ), f"{map_index} does not correspond to an atom in the molecule."
+
+        return atom_index
 
     def start_message(self, **kwargs) -> str:
         """
